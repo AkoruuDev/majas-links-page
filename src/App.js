deea6c40f4e63e387c301698a67f03b85a297c4f
AkoruuDev <akoruu.dev@gmail.com>
2023-07-28 21:25:57 -0300

    feat: update App path page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyles/globalStyle";
-import Main from "./Pages/Main";
+// import Main from "./Pages/Main";
 import Index from "./Pages/Main/Index"
 import Home from "./Pages/Home";
+import { Admin, SignIn, SignUp } from "./Pages/admin";
 
 function App() {
   return (
@@ -11,8 +12,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={ <Home /> } />
-          <Route path="/elements" element={ <Main /> } />
+          {/* <Route path="/contact" element={ <Main /> } /> */}
           <Route path="/contact" element={ <Index /> } />
+          
+          <Route path="/admin" element={ <Admin /> } />
+          <Route path="/admin/sign-in" element={ <SignIn /> } />
+          <Route path="/admin/sign-up" element={ <SignUp /> } />
         </Routes>
       </BrowserRouter>
     </>
